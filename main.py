4b2c334bde95d215b675796131bab02d85567c16
Alexander <alucenko@nes.ru>
2022-05-13 20:20:23 +0300

    f

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
-import streamlit as st, pandas as pd, numpy as np
-"""
-график прикольный
-"""
-x = np.linspace(-8, 8, 1000)
-df = pd.DataFrame(dict(y=np.sin(x)))
-st.line_chart(df)
-"""
-привет нет блин пока
-"""
+import numpy as np
+import pandas as pd
+import streamlit as st
+import mathplotlib.pyplot as plt
+with st.echo(code_location='below'):
+    """
+    график прикольный
+    """
+    x = np.linspace(-8, 8, 1000)
+    df = pd.DataFrame(dict(y=np.sin(x)))
+    st.line_chart(df)
+    """
+    привет нет блин пока
+    """
