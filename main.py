f3a2805ca9106456c3f384e7c735ced10500a63e
Alexander <alucenko@nes.ru>
2022-05-14 15:12:53 +0300

    Updates

--- main.py
+++ main.py
@@ -6,75 +6,74 @@ import matplotlib.pyplot as plt
 import altair as alt
 
 with st.echo(code_location='below'):
-    """
-    ## Hello, World!
-    """
+    SpotifyData = pd.read_csv('Spotify_data.csv')
+    df = SpotifyData
+    st.sidebar.title("🎶 Музыка 2010-х")
+    visualization = st.sidebar.radio("Выберите визуализацию:", ["Тренды в музыке", "Особенности песен", "Топ жанров",
+                                                                "Топ артистов", "Датасет"])
+
+    charact_codes = ["bpm", "nrgy", "dnce", "dB", "dur", "live", "val", "acous", "spch"]
+    charact_list = ["Темп (bpm)", "Энергия", "Танцевальность", "Громкость", "Длительность (в секундах)",
+                    "Живое исполнение", "Жизнерадостность", "Акустичность", "Речитативность"]
+    df = df.rename(columns=dict(zip(charact_codes, charact_list)))
+    for ch in ["Энергия", "Танцевальность", "Громкость",       # нормализация характеристик
+                    "Живое исполнение", "Жизнерадостность", "Акустичность", "Речитативность"]:
+        df[ch] = 100 * (df[ch] - df[ch].quantile(0.1)) / (df[ch].quantile(0.9) - df[ch].quantile(0.1))
 
 
-    def print_hello(name="World"):
-        st.write(f"### Hello, {name}!")
+    match visualization:
 
+        case "Тренды в музыке":
+            st.markdown("# Тренды в музыке")
+            st.markdown("### В течение 2010-х годов слушатели стали выше ценить как танцевальные и "
+                        "речитативные, так и акустические композиции, а средняя длительность и энергичность популярных "
+                        "песен сократилась.")
+            st.write("Ежегодно стриминговая платформа Spotify определяет 100 наиболее прослушиваемых песен. "
+                     "Визуализация показывает, как на протяжении 2010–2019 годов изменялись их средние характеристики. "
+                     "Музыкальные особенности, такие как энергия и танцевальность, определены алгоритмами Spotify и "
+                     "приведены к шкалам, в которых большинство песен характеризуется значениями от 0 до 100. "
+                     )
 
-    name = st.text_input("Your name", key="name", value="Anonymous")
-    print_hello(name)
-
-    """
-    ## Добавим графики
-    Чтобы заработали библиотеки seaborn и altair, нужно добавить в проект файл 
-    `requirements.txt` с такими строчками:
-
-        seaborn
-        altair
-    """
-
-    a = st.slider("a")
-    x = np.linspace(-6, 6, 500)
-    df = pd.DataFrame(dict(x=x, y=np.sin(a * x)))
-    fig, ax = plt.subplots()
-    sns.lineplot(data=df, x="x", y="y", ax=ax)
-    st.pyplot(fig)
-
-    """
-    ## Немного анализа данных
-    """
-
-
-    @st.cache
-    def get_data():
-        data_url = (
-            "https://github.com/Godoy/imdb-5000-movie-dataset/raw/"
-            "master/data/movie_metadata.csv"
-        )
-        return (
-            pd.read_csv(data_url)
-                .dropna(subset=["title_year"])
-                .assign(
-                title_year=lambda x: pd.to_datetime(
-                    x["title_year"], format="%Y"
+            characteristics = np.array(st.multiselect("Выберите одну или несколько характеристик:", charact_list,
+                                                      default=["Танцевальность", "Акустичность",
+                                                               "Длительность (в секундах)"]))
+            source = pd.DataFrame()
+            for ch in characteristics:
+                source[ch] = df.groupby("top year")[ch].mean()
+            try:
+                source = pd.melt(source.reset_index(), "top year")
+                highlight = alt.selection(type='single', on='mouseover',
+                                          fields=['variable'], nearest=True)
+                base = alt.Chart(source).encode(
+                    alt.X("top year:O", title="Год"),
+                    alt.Y("value:Q", title="Среднее значение"),
+                    alt.Color("variable", title="Характеристика")
                 )
-            )
-        )
+                ### FROM: https://altair-viz.github.io/gallery/multiline_highlight.html
+                points = base.mark_circle().encode(
+                    opacity=alt.value(0)
+                ).add_selection(
+                    highlight
+                ).properties(
+                    width=800,
+                    height=500
+                )
+                lines = base.mark_line(interpolate="basis").encode(
+                    size=alt.condition(~highlight, alt.value(2), alt.value(4))
+                )
+                ### END FROM
+                st.altair_chart((points + lines).interactive())
+            except:
+                st.write("Выберите хотя бы одну характеристку.")
 
 
-    df = get_data()
 
-    director = st.selectbox(
-        "Director", df["director_name"].value_counts().iloc[:10].index
-    )
+        case "Датасет":
+            st.write(SpotifyData)
 
-    df_selection = df[lambda x: x["director_name"] == director]
-    df_selection
-
-    chart = (
-        alt.Chart(df_selection)
-            .mark_circle()
-            .encode(x=alt.X("title_year:T"), y="imdb_score", tooltip="movie_title")
-    )
-
-    st.altair_chart(
-        (
-                chart
-                + chart.transform_loess("title_year", "imdb_score").mark_line()
-        ).interactive()
-        # .transform_loess добавляет сглаживающую кривую
-    )
+    st.markdown("***")
+    """
+    Автор базы данных — [Michael Morris](https://www.kaggle.com/datasets/muhmores/spotify-top-100-songs-of-20152019).
+    
+    Исходный код:
+    """
